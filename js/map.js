d903feabf8817ccd71af31f14a67b4e93352c55b
salvadorc94 <secg.1994@gmail.com>
2020-07-04 19:48:55 -0600

    Added geojson file

--- js/map.js
+++ js/map.js
@@ -1,18 +1,18 @@
-/*var casos = L.layerGroup();
-fetch('http://babel.webreactiva.com/labs/arboles_singulares_en_espacios_naturales.geojson')
+var casos = L.layerGroup();
+fetch('https://raw.githubusercontent.com/salvadorc94/geoJson/master/cords.geojson')
 .then(
     res => res.json()
 ).then(
     data => {
         let geojsonlayer = L.geoJSON(data, {
             onEachFeature: function(feature, layer){
-                layer.bindPopup(feature.properties['arbol_nombre'])
+                layer.bindPopup(feature.properties['text'])
             }
         }).addTo(casos)
     }
-)*/
+)
 
-var casos = L.layerGroup();
+/*var casos = L.layerGroup();
 L.marker([13.735383, -89.201088]).bindPopup("Ayutuxtepeque: 18").addTo(casos);
 L.marker([13.694108, -89.109249]).bindPopup("Ilopango: 80").addTo(casos);
 L.marker([13.737718, -89.055519]).bindPopup("San Martin: 60").addTo(casos);
@@ -26,7 +26,7 @@ L.marker([13.800407, -89.177742]).bindPopup("Apopa: 81").addTo(casos);
 L.marker([13.722876, -89.171219]).bindPopup("Ciudad Delgado: 107").addTo(casos);
 L.marker([13.723043, -89.18787]).bindPopup("Mejicanos: 135").addTo(casos);
 L.marker([13.673343, -89.286232]).bindPopup("Santa Tecla: 103").addTo(casos);
-L.marker([13.727837, -89.180961]).bindPopup("Cuscatancingo: 42").addTo(casos);
+L.marker([13.727837, -89.180961]).bindPopup("Cuscatancingo: 42").addTo(casos);*/
 
 var mbAttr = 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
         '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
